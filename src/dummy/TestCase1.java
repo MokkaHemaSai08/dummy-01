645e07101cb16b25ba3d976adedb59b308193cdb
MokkaHemaSai08 <Mokka.HemaSai@cognizant.com>
2025-07-08 10:34:20 +0530

    Update TestCase1.java

--- src/dummy/TestCase1.java
+++ src/dummy/TestCase1.java
@@ -5,7 +5,7 @@ public class TestCase1 {
 	public static void main(String[] args) {
 
 		System.out.println("this is a test case1.");
-		System.out.println();
+		System.out.println("this is a testing environment.");
 	}
 
 }
